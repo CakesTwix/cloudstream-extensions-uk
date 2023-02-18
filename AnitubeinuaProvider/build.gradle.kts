04dd470753df0218ba878e4717f10c3d413e7bf3
CakesTwix <oleg.kiryazov@gmail.com>
2023-02-18 15:13:46 +0200

    Anitubeinua: fork from Eneyida

---
+++ AnitubeinuaProvider/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "uk"
+    // All of these properties are optional, you can safely remove them
+
+    description = "На нашому сайті ви можете подивитися аніме онлайн українською безкоштовно. Великий список аніме онлайн тільки у нас."
+    authors = listOf("CakesTwix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 3 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=anitube.in.ua&sz=%size%"
+}
