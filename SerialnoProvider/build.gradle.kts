66145e6e85dd001e040b574af73aae7d7ea18378
CakesTwix <oleg.kiryazov@gmail.com>
2023-02-21 10:56:45 +0200

    serialno: done

---
+++ SerialnoProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "uk"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Відкриваємо світ серіалів, фентезі, містики, екшну, драми та ще безлічі інших жанрів українською мовою - все це Серіально.ТВ. Можна багато чого писати, але сенсу особливо немає. Ви тут, а значить прийшли дивитися серіали. То ж, вперед!:)"
+    authors = listOf("CakesTwix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Cartoon",
+        "TvSeries",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=serialno.tv&sz=%size%"
+}
---
+++ SerialnoProvider/src/main/kotlin/com/lagradost/SerialnoProvider.kt
@@ -0,0 +1,162 @@
+package com.lagradost
+
+import android.util.Log
+import com.lagradost.cloudstream3.*
+import com.lagradost.models.PlayerJson
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.M3u8Helper
+import com.lagradost.cloudstream3.utils.Qualities
+import org.jsoup.nodes.Element
+
+class SerialnoProvider : MainAPI() {
+
+    // Basic Info
+    override var mainUrl = "https://serialno.tv"
+    override var name = "Serialno"
+    override val hasMainPage = true
+    override var lang = "uk"
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(
+        TvType.Movie,
+        TvType.Cartoon,
+        TvType.TvSeries,
+        TvType.Anime
+    )
+
+    // Sections
+    override val mainPage = mainPageOf(
+        "$mainUrl/series/page/" to "Серіали",
+        "$mainUrl/cartoons/page/" to "Мультсеріали",
+        "$mainUrl/mini-serials/page/" to "Міні-серіали",
+
+        )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val document = app.get(request.data + page).document
+
+        val home = document.select(".th-item").map {
+            it.toSearchResponse()
+        }
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResponse(): SearchResponse {
+        val title = this.select(".th-title").text()
+        val href = this.select(".th-in").attr("href")
+        val posterUrl = mainUrl + this.select(".img-fit img").attr("data-src")
+
+        return newMovieSearchResponse(title, href, TvType.TvSeries) {
+            this.posterUrl = posterUrl
+        }
+
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document = app.post(
+            url = "$mainUrl/index.php?do=search",
+            data = mapOf(
+                "do" to "search",
+                "subaction" to "search",
+                "story" to query.replace(" ", "+")
+            )
+        ).document
+
+        return document.select(".th-item").map {
+            it.toSearchResponse()
+        }
+    }
+
+    // Detailed information
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+
+        // General info
+        val generalInfo = document.select(".flist li")
+
+        // Parse info
+        val title = document.select(".full h1").text()
+        val poster = document.select(".fposter a").attr("href")
+
+        // Can be smaller
+        if (generalInfo.size > 4) {
+            var tags = document.select(".flist li")[4].select("a").map { it.text() }
+        } else {
+            var tags = document.select(".flist li")[3].select("a").map { it.text() }
+        }
+        val year = document.select(".flist li")[1].select("a").text().toIntOrNull()
+        var tvType = TvType.TvSeries
+        val description = document.select(".full-text").text()
+        // val author = someInfo.select("strong:contains(Студія:)").next().html()
+        val rating = document.selectFirst(".th-voice")?.text().toRatingInt()
+
+        // Parse episodes
+        var episodes: List<Episode> = emptyList()
+        val playerUrl = document.select("div.video-box iframe").attr("src")
+
+        // Return to app
+        // Parse Episodes as Series
+        val playerRawJson = app.get(playerUrl).document.select("script").html()
+            .substringAfterLast("file:\'")
+            .substringBefore("\',")
+
+        AppUtils.tryParseJson<List<PlayerJson>>(playerRawJson)?.map { dubs -> // Dubs
+            for (season in dubs.folder) {                                     // Seasons
+                for (episode in season.folder) {                              // Episodes
+                    episodes = episodes.plus(
+                        Episode(
+                            "${season.title}, ${episode.title}, $playerUrl",
+                            episode.title,
+                            season.title.replace(" Сезон ", "").toIntOrNull(),
+                            episode.title.replace("Серія ", "").toIntOrNull(),
+                            episode.poster
+                        )
+                    )
+                }
+            }
+        }
+        return newTvSeriesLoadResponse(title, url, tvType, episodes) {
+            this.posterUrl = poster
+            this.year = year
+            this.plot = description
+            this.tags = tags
+            this.rating = rating
+        }
+    }
+
+
+    // It works when I click to view the series
+    override suspend fun loadLinks(
+        data: String, // (Serial) [Season, Episode, Player Url]
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val dataList = data.split(", ")
+
+        val playerRawJson = app.get(dataList[2]).document.select("script").html()
+            .substringAfterLast("file:\'")
+            .substringBefore("\',")
+
+        AppUtils.tryParseJson<List<PlayerJson>>(playerRawJson)?.map { dubs ->   // Dubs
+            for(season in dubs.folder){                                // Seasons
+                if(season.title == dataList[0]){
+                    for(episode in season.folder){                     // Episodes
+                        if(episode.title == dataList[1]){
+                            // Add as source
+                            M3u8Helper.generateM3u8(
+                                source = dubs.title,
+                                streamUrl = episode.file,
+                                referer = "https://tortuga.wtf/"
+                            ).forEach(callback)
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
