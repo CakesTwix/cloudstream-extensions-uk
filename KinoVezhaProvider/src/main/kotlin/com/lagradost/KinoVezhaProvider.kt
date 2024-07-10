df8f61020125ac57795af9ba26819f6366517279
CakesTwix <oleg.kiryazov@gmail.com>
2024-07-10 12:45:55 +0300

    kinovezha: Drop some imports

--- KinoVezhaProvider/src/main/kotlin/com/lagradost/KinoVezhaProvider.kt
+++ KinoVezhaProvider/src/main/kotlin/com/lagradost/KinoVezhaProvider.kt
@@ -1,12 +1,10 @@
 package com.lagradost
 
-import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.models.PlayerJson
 import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper
-import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Element
 
 class KinoVezhaProvider : MainAPI() {
