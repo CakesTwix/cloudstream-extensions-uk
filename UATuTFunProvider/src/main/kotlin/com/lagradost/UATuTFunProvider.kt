380d6f69e6531fc2186fcaf236ef18f778a62472
Partizanin <topiciv1@yandex.ua>
2025-02-18 16:53:52 +0200

    implementing search function

--- UATuTFunProvider/src/main/kotlin/com/lagradost/UATuTFunProvider.kt
+++ UATuTFunProvider/src/main/kotlin/com/lagradost/UATuTFunProvider.kt
@@ -1,11 +1,11 @@
 ﻿package com.lagradost
 
-import com.lagradost.api.Log
 import com.lagradost.cloudstream3.ErrorLoadingException
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.MovieSearchResponse
+import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.mainPageOf
@@ -20,6 +20,7 @@ class UATuTFunProvider : MainAPI() {
     private val videoUrlSelector = "data-url"
     private val posterUrlSelector =
         "div.poster__img.img-responsive.img-responsive--portrait.img-fit-cover.anim > img"
+    private val searchMovieSelector = "div.poster.grid-item"
 
     // Basic Info
     override var mainUrl = "https://uk.uatut.fun"
@@ -27,6 +28,7 @@ class UATuTFunProvider : MainAPI() {
     override val hasMainPage = true
     override var lang = "uk"
     override val hasDownloadSupport = false
+
     override val supportedTypes = setOf(
         TvType.TvSeries,
         TvType.Cartoon,
@@ -49,13 +51,12 @@ class UATuTFunProvider : MainAPI() {
             .first() ?: throw ErrorLoadingException("Can't find main page")
 
         val mainPage = first.children().map {
-            it.getVideoData()
+            it.toSearchResponse()
         }
         return newHomePageResponse(request.name, mainPage)
     }
 
-
-    private fun Element.getVideoData(): MovieSearchResponse {
+    private fun Element.toSearchResponse(): MovieSearchResponse {
         val title = this.select(titleSelector).text()
         val url = this.attr(videoUrlSelector)
         val posterUrl = mainUrl +
@@ -64,6 +65,16 @@ class UATuTFunProvider : MainAPI() {
         return newMovieSearchResponse(title, url, TvType.Movie) {
             this.posterUrl = posterUrl
         }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val response =
+            app.post("$mainUrl/index.php?do=search&subaction=search&story=$query").document
+        val select = response.select(searchMovieSelector)
+        val map = select.map {
+            it.toSearchResponse()
+        }
+        return map
     }
 
 }
