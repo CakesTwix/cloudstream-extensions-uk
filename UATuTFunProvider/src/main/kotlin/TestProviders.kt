70fd5723657bc2a7560f20baca84d08a1dbfc025
Partizanin <topiciv1@yandex.ua>
2025-02-18 13:44:04 +0200

    fix poster url

--- UATuTFunProvider/src/main/kotlin/TestProviders.kt
+++
@@ -1,6 +0,0 @@
-import com.lagradost.UATuTFunProvider
-
-suspend fun main() {
-    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(UATuTFunProvider())
-    providerTester.testMainPage()
-}
