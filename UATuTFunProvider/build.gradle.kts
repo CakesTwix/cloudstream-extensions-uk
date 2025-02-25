05fce4f4145fb8086902a768afb9ce9450c5d491
Partizanin <topiciv1@yandex.ua>
2025-02-25 12:09:30 +0200

    change status

--- UATuTFunProvider/build.gradle.kts
+++ UATuTFunProvider/build.gradle.kts
@@ -19,7 +19,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
      * */
-    status = 1 // will be 3 if unspecified
+    status = 3 // will be 3 if unspecified
 
     iconUrl = "https://www.google.com/s2/favicons?domain=uatut.fun&sz=%size%"
 }
